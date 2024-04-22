f077aa468227ec13a36d70961f622fd8f9db1613
michaelitindi <michaelitindi@gmail.com>
2024-04-22 07:39:07 +0300

    wrote initial animals class

--- src/Animal.js
+++ src/Animal.js
@@ -1,4 +1,4 @@
-class Animal{
+export default class Animal{
     constructor(name,age,gender){
         this.name = name;
         this.age = age;
@@ -23,3 +23,4 @@ class Animal{
         this.gender = gender;
     }
 }
+
