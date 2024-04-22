7ed0fadc2fcf2e1d4b108de96b9cf8167c617649
michaelitindi <michaelitindi@gmail.com>
2024-04-22 07:58:22 +0300

    overode sound function using cat

---
+++ src/Cat.js
@@ -0,0 +1,12 @@
+import Animal from "./Animal";
+class Cat extends Animal{
+    constructor(name,age,gender)
+    {
+        super(name,age,gender)
+    }
+
+sound()
+{
+    console.log("Meow")
+}
+}
